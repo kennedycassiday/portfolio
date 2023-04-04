09bfd8a8e895918f43eb0142977119e1166db89f
Kennedy <kennedycassiday@gmail.com>
2023-04-04 11:18:11 -0700

    fixed linting errors

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import { Jumbotron } from "./migration";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
